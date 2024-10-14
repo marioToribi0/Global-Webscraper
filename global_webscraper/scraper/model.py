af62791a12345199efa4c8529441dd7e2e751c3c
Mario Toribio <yuniormtr@gmail.com>
2024-10-14 16:25:38 -0400

    feat: scrap-websites

--- global_webscraper/scraper/model.py
+++ global_webscraper/scraper/model.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import List
+from global_webscraper.scraper.utilities import extract_url_webpage, validate_url_webpage, extract_chunks
+import functools
+import operator
 
 class WebScraper:
-    pass
+    @staticmethod
+    def scrap_website(base_url: str, max_extra_webpages: int = 0) -> List[str]:
+        """Scrap website with all on a tag on the page"""
+        page = f"{base_url}"        
+        request = requests.get(page)
+        soup = BeautifulSoup(request.text, "html.parser")
+        
+        urls = validate_url_webpage(extract_url_webpage(soup), base_url)[:max_extra_webpages]
+        
+        urls.append(base_url)
+        
+        chunks = [extract_chunks(BeautifulSoup(requests.get(url).text, "html.parser")) for url in urls]
 
-if __name__ == "__main__":
-    page = f"https://www.unibe.edu.do"
-    
-    request = requests.get(page)
-    soup = BeautifulSoup(request.text, "html.parser")
-    
-    print(soup.text)
+        chunks = functools.reduce(operator.iconcat, chunks, [])
+        
+        return chunks
--- global_webscraper/scraper/utilities.py
+++ global_webscraper/scraper/utilities.py
@@ -1,5 +1,6 @@
 from urllib.parse import urljoin, urlparse
 from typing import List
+import re
 from bs4 import BeautifulSoup
 
 def is_valid_url(url: str) -> str:
@@ -23,3 +24,9 @@ def extract_chunks(soup: BeautifulSoup, chunks: int = 300) -> List[str]:
     for i in range(len(content)//chunks+1):
         batches.append(" ".join(content[i*chunks:(i+1)*chunks]))
     return batches
+
+def validate_url_webpage(list_url: List[str], base_url: str) -> List[str]:
+    """Extract all url valid from a specific webpage"""
+    pages = list(filter(lambda url: re.search(rf"{base_url}.*", url), list_url))
+    
+    return list(set(pages))
